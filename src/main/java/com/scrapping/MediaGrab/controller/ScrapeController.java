4a4d1908c1568807db53ff67321aa2e5ac00a23b
Rahul Singh <rahul660singh@gmail.com>
2025-12-02 16:19:07 +0530

    Controller layer development

--- src/main/java/com/scrapping/MediaGrab/controller/ScrapeController.java
+++ src/main/java/com/scrapping/MediaGrab/controller/ScrapeController.java
@@ -1,9 +1,23 @@
 package com.scrapping.MediaGrab.controller;
 
+import com.scrapping.MediaGrab.model.ScrapeResponse;
+import com.scrapping.MediaGrab.service.ScraperService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class ScrapeController {
 
+    @Autowired
+    private ScraperService scraperService;
 
+    @GetMapping("/api/scrape")
+    public ScrapeResponse scrape(@RequestParam String url) throws IOException {
+        // Calling this method from the service class.
+        return  scraperService.webScraper(url);
+    }
 }
